53caef6b25af586bcf05e47ae29e8591581c92a4
Kirill Kamakin <k.kamakin@protonmail.ch>
2018-09-27 17:26:30 +0300

    Add observable collection

--- src/main/java/kirmanak/TextSearcher/EntryPoint.java
+++ src/main/java/kirmanak/TextSearcher/EntryPoint.java
@@ -1,26 +1,32 @@
 package kirmanak.TextSearcher;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.message.EntryMessage;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 @Log4j2
 public class EntryPoint extends Application {
+    private final ObservableList<MarkedFile> FILES =
+            FXCollections.synchronizedObservableList(FXCollections.observableList(new ArrayList<>()));
     private final TextField PATH_FIELD = new TextField();
     private final TextField TEXT_FIELD = new TextField();
-    private final TextField EXTENSION_FIELD = new TextField();
-    private final Text TEXT = new Text();
+    private final TextField EXTENSION_FIELD = new TextField("log");
+    private final TextArea TEXT = new TextArea();
 
     public static void main(String[] args) {
         launch();
@@ -28,6 +34,20 @@ public class EntryPoint extends Application {
 
     public void start(final Stage primaryStage) {
         final EntryMessage entryMessage = log.traceEntry("start(primaryStage = {}) of {}", primaryStage, this);
+        FILES.addListener(this::updateView);
+        primaryStage.setTitle("TextSearcher");
+        primaryStage.setScene(constructScene());
+        primaryStage.show();
+        log.traceExit(entryMessage);
+    }
+
+    /**
+     * Constructs the main scene to be displayed
+     *
+     * @return the constructed Scene
+     */
+    private Scene constructScene() {
+        final EntryMessage entryMessage = log.traceEntry("constructScene() of {}", this);
         final Button actionButton = new Button("Search");
         actionButton.setOnAction(this::onSearchRequest);
         final GridPane gridPane = new GridPane();
@@ -37,10 +57,7 @@ public class EntryPoint extends Application {
         gridPane.addRow(3, new Label("Text:"), TEXT_FIELD);
         gridPane.add(actionButton, 1, 4);
         final Scene scene = new Scene(gridPane);
-        primaryStage.setTitle("TextSearcher");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        log.traceExit(entryMessage);
+        return log.traceExit(entryMessage, scene);
     }
 
     /**
@@ -74,7 +91,7 @@ public class EntryPoint extends Application {
     private void search(final TextSearcher searcher) {
         final EntryMessage entryMessage = log.traceEntry("search(searcher = {}) of {}", searcher, this);
         try {
-            searcher.search(this::showFile);
+            searcher.search(FILES::add);
         } catch (final IOException e) {
             log.error(entryMessage, e);
         }
@@ -82,15 +99,12 @@ public class EntryPoint extends Application {
     }
 
     /**
-     * Shows the content of found file in the GUI
+     * Updates the content view in the GUI
      *
-     * @param file file to show
+     * @param change change happened in the list
      */
-    private void showFile(final MarkedFile file) {
-        final EntryMessage entryMessage = log.traceEntry("showFiles(file = {}) of {}", file, this);
-        final StringBuilder builder = new StringBuilder();
-        file.getLines().stream().map((line) -> String.format("%s%n", line)).forEach(builder::append);
-        TEXT.setText(builder.toString());
+    private void updateView(final ListChangeListener.Change<? extends MarkedFile> change) {
+        final EntryMessage entryMessage = log.traceEntry("updateView(change = {}) of {}", change, this);
         log.traceExit(entryMessage);
     }
 }

3a525788260001cd9a5b459bd8054b9d15e73037
Kirill Kamakin <k.kamakin@protonmail.ch>
2018-09-30 12:23:00 +0300

    Make Tasks sub-classes of Services

--- src/main/java/kirmanak/TextSearcher/MarkedFileService.java
+++ src/main/java/kirmanak/TextSearcher/MarkedFileService.java
@@ -6,8 +6,12 @@ import javafx.scene.control.TextArea;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.message.EntryMessage;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Iterator;
 
 @Log4j2
 @Getter
@@ -19,5 +23,55 @@ public class MarkedFileService extends Service<TextArea> {
     @Override
     protected Task<TextArea> createTask() {
         return new MarkedFileTask(getPath(), getText());
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public class MarkedFileTask extends Task<TextArea> {
+        private final Path path;
+        private final String text;
+
+        @Override
+        protected TextArea call() throws Exception {
+            final EntryMessage m = log.traceEntry("call() of {}", this);
+
+            return log.traceExit(m, readText(initializeTextArea()));
+        }
+
+        /**
+         * Creates and configures a TextArea instance
+         *
+         * @return
+         */
+        private TextArea initializeTextArea() {
+            final EntryMessage m = log.traceEntry("initializeTextArea() of {}", this);
+            final TextArea textArea = new TextArea();
+            textArea.setEditable(false);
+            return log.traceExit(m, textArea);
+        }
+
+        /**
+         * Reads text to the provided TextArea from the file
+         *
+         * @param textArea the initialized TextArea instance
+         * @return the provided text area containing the file content
+         * @throws IOException if any I/O error has happened
+         */
+        private TextArea readText(final TextArea textArea) throws IOException {
+            final EntryMessage m = log.traceEntry("readText(textArea = {}) of {}", textArea, this);
+            final StringBuilder sb = new StringBuilder();
+            final Iterator<String> lineIterator = Files.lines(getPath()).iterator();
+            int counter = 0;
+            while (lineIterator.hasNext()) {
+                final String line = lineIterator.next();
+                sb.append(line).append("\n");
+                if (line.contains(getText())) {
+                    // TODO: highlight the text
+                }
+                counter++;
+            }
+            textArea.setText(sb.toString());
+            return log.traceExit(m, textArea);
+        }
     }
 }
--- src/main/java/kirmanak/TextSearcher/TextSearchService.java
+++ src/main/java/kirmanak/TextSearcher/TextSearchService.java
@@ -3,12 +3,20 @@ package kirmanak.TextSearcher;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.message.EntryMessage;
 
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 @Getter
 @Log4j2
@@ -46,5 +54,86 @@ public class TextSearchService extends Service<List<Path>> {
     @Override
     protected Task<List<Path>> createTask() {
         return new TextSearchTask(getRootFolder(), getExtension(), getText());
+    }
+
+    public String getExtension() {
+        return String.format(".%s", extension);
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    private class TextSearchTask extends Task<List<Path>> {
+        private final Path rootFolder;
+        private final String extension;
+        private final String text;
+
+        @Override
+        protected List<Path> call() throws IOException {
+            final EntryMessage m = log.traceEntry("call() of {}", this);
+            updateMessage("Looking for files...");
+            final List<Future<Optional<Path>>> futures = walk();
+            updateMessage("Reading files...");
+            final List<Path> result = getResult(futures);
+            updateMessage("Done");
+            return log.traceExit(m, result);
+        }
+
+        /**
+         * Calls future.get() and handles the result
+         *
+         * @param futures the futures to be unwrapped
+         * @return the unwrapping results
+         */
+        private List<Path> getResult(final List<Future<Optional<Path>>> futures) {
+            final EntryMessage m = log.traceEntry("getResult(futures = {}) of {}", futures, this);
+            final int FILE_COUNT = futures.size();
+            final ArrayList<Path> result = new ArrayList<>(FILE_COUNT);
+            int counter = 0;
+            for (final Future<Optional<Path>> future : futures) {
+                try {
+                    future.get().ifPresent(result::add);
+                } catch (final InterruptedException err) {
+                    log.error(m, err);
+                    Thread.currentThread().interrupt();
+                } catch (final ExecutionException err) {
+                    log.error(m, err);
+                }
+                counter++;
+                updateProgress(counter, FILE_COUNT);
+            }
+            result.trimToSize();
+            return log.traceExit(m, result);
+        }
+
+        /**
+         * Creates a callable which tests whether the provided file has the required text
+         *
+         * @param path the path to be checked
+         * @return the callable to be executed
+         */
+        private Callable<Optional<Path>> pathToCallable(final Path path) {
+            final EntryMessage m = log.traceEntry("pathToCallable(path = {})", path);
+            return log.traceExit(m, () -> {
+                final EntryMessage callableEntryMessage = log.traceEntry("anonymous(path = {})", path);
+                final boolean result = Files.lines(path).anyMatch(line -> line.contains(getText()));
+                return log.traceExit(callableEntryMessage, result ? Optional.of(path) : Optional.empty());
+            });
+        }
+
+        /**
+         * Walks through the file system starting from the root path
+         *
+         * @return list of futures with results of the file reading
+         * @throws IOException if any I/O error has happened
+         */
+        private List<Future<Optional<Path>>> walk() throws IOException {
+            final EntryMessage m = log.traceEntry("walk() of {}", this);
+            final ExecutorService executorService = Executors.newWorkStealingPool();
+            return log.traceExit(m, Files.walk(getRootFolder(), FileVisitOption.FOLLOW_LINKS)
+                    .filter(path -> path.toString().endsWith(getExtension()))
+                    .map(this::pathToCallable)
+                    .map(executorService::submit)
+                    .collect(Collectors.toList()));
+        }
     }
 }
